36cbda6366d4cbc6aa5e5acf0a89573210cdc45c
makarov_va <makarov_va@nectech.pro>
2026-06-05 13:16:33 +0300

    feat: yandex verification

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -28,3 +29,13 @@ async def index(request: Request):
             "title": "TAKE52 — видеосъёмка в Нижнем Новгороде",
         },
     )
+@app.get("/yandex_5ce173d08fc739d.html", response_class=HTMLResponse)
+async def yandex_verification():
+    return """
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>Verification: 5ce173d08fc739d</body>
+</html>
+"""
